8068139270a10703f913b45cdc59e90afee3f169
Azat Nasyrov <azat.nasyrov31@gmail.com>
2022-02-23 16:13:04 +0600

    Improved product creation form

--- client/src/components/ProductForm/ProductForm.js
+++ client/src/components/ProductForm/ProductForm.js
@@ -1,9 +1,8 @@
 import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import FileInput from '../UI/Form/FileInput';
-import MenuItem from '@material-ui/core/MenuItem';
+import FormElement from '../UI/Form/FormElement';
 
 const ProductForm = ({onSubmit, categories}) => {
   const [state, setState] = useState({
@@ -49,66 +48,40 @@ const ProductForm = ({onSubmit, categories}) => {
   return (
     <form onSubmit={submitFormHandler}>
       <Grid container direction="column" spacing={2}>
-        <Grid item xs>
-          <TextField
-            required
-            fullWidth
-            select
-            variant="outlined"
-            label="Category"
-            name="category"
-            value={state.category}
-            onChange={inputChangeHandler}
-          >
-            <MenuItem><i>Select a category</i></MenuItem>
-            {categories.map(category => (
-              <MenuItem
-                key={category._id}
-                value={category._id}
-              >
-                {category.title}
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-        <Grid item xs>
-          <TextField
-            required
-            fullWidth
-            variant="outlined"
-            id="title"
-            label="Title"
-            name="title"
-            value={state.title}
-            onChange={inputChangeHandler}
-          />
-        </Grid>
-        <Grid item xs>
-          <TextField
-            required
-            fullWidth
-            variant="outlined"
-            type="number"
-            id="price"
-            label="Price"
-            name="price"
-            value={state.price}
-            onChange={inputChangeHandler}
-          />
-        </Grid>
-        <Grid item xs>
-          <TextField
-            fullWidth
-            multiline
-            rows={3}
-            variant="outlined"
-            id="description"
-            label="Description"
-            name="description"
-            value={state.description}
-            onChange={inputChangeHandler}
-          />
-        </Grid>
+        <FormElement
+          required
+          select
+          label="Category"
+          name="category"
+          value={state.category}
+          onChange={inputChangeHandler}
+          options={categories}
+        />
+        <FormElement
+          required
+          label="Title"
+          name="title"
+          value={state.title}
+          onChange={inputChangeHandler}
+        />
+        <FormElement
+          required
+          type="number"
+          label="Price"
+          name="price"
+          value={state.price}
+          onChange={inputChangeHandler}
+        />
+        <FormElement
+          required
+          multiline
+          rows={3}
+          label="Description"
+          name="description"
+          value={state.description}
+          onChange={inputChangeHandler}
+        />
+
         <Grid item xs>
           <FileInput
             name="image"
@@ -116,6 +89,7 @@ const ProductForm = ({onSubmit, categories}) => {
             onChange={fileChangeHandler}
           />
         </Grid>
+
         <Grid item xs>
           <Button type="submit" color="primary" variant="contained">
             Create
--- client/src/components/UI/Form/FormElement.js
+++ client/src/components/UI/Form/FormElement.js
@@ -2,34 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const FormElement = ({required, name, label, value, onChange, error, type, autoComplete}) => {
+const FormElement = ({error, select, options, ...props}) => {
+  let inputChildren = null;
+
+  if (select) {
+    inputChildren = options.map(option => (
+      <MenuItem key={option._id} value={option._id}>
+        {option.title}
+      </MenuItem>
+    ));
+  }
+
   return (
     <Grid item xs>
       <TextField
-        required={required}
-        type={type}
-        label={label}
-        name={name}
-        autoComplete={autoComplete}
-        value={value}
+        select={select}
         error={Boolean(error)}
         helperText={error}
-        onChange={onChange}
-      />
+        {...props}
+      >
+        {inputChildren}
+      </TextField>
     </Grid>
   );
 };
 
 FormElement.propTypes = {
-  required: PropTypes.string,
-  type: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  autoComplete: PropTypes.string,
-  value: PropTypes.any.isRequired,
-  onChange: PropTypes.func.isRequired,
+  ...TextField.propTypes,
   error: PropTypes.string,
+  select: PropTypes.bool,
+  options: PropTypes.arrayOf(PropTypes.object)
 };
 
 export default FormElement;
